d92d80d674c582380246124a13a60af0f020d137
Antonio <tenlla2@gmail.com>
2024-03-03 19:33:38 +0100

    feat(Button): add component

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Button.css'
+
+export interface ButtonProps {
+  text: string
+  className: string
+  onClick: () => void
+  disabled?: boolean
+}
+
+export const Button: React.FC<ButtonProps> = ({ text, className, onClick, disabled = false }) => {
+  return (
+    <button className={`custom-button ${className}`} onClick={onClick} disabled={disabled}>
+      {text}
+    </button>
+  )
+}
