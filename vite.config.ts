5e24306a93843d22e644f94de11226d806e8857d
Antonio <tenlla2@gmail.com>
2024-03-04 13:53:19 +0100

    fix(vite.config): correct build format

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),dts({
+    insertTypesEntry: true,
+  }),],
   resolve: {
     alias: [{ find: '@', replacement: resolve(__dirname, 'src') }],
   },
@@ -21,5 +24,23 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: './src/test/setup.ts',
     css: true,
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/components/index.ts"),
+      name: "tenllado-lib",
+      formats: ["es", "umd"],
+      fileName: (format) => `tenllado-lib.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'styled-components'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          'styled-components': 'styled',
+        },
+      },
+    },
   },
 })
